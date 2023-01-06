4ad654fa1d74e206988b9598a1c0c292f3688c64
ipcampbell12 <ipcampbell12@gmail.com>
2023-01-06 12:33:03 -0800

    average

---
+++ average.py
@@ -0,0 +1,16 @@
+
+my_grades = [
+    {
+        "score": 1
+    },
+    {
+        "score": 4
+    },
+    {
+        "score": 5
+    }
+]
+
+scores = [score["score"] for score in my_grades]
+
+print(scores)
